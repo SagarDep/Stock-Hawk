e119b82b8c0462c9af09ce42b6987547c425471f
Victor Akap Oben <aoben@gatech.edu>
2016-11-07 15:31:17 -0500

    Stocks can be displayed in a collections widget

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StocksWidgetProvider.java
@@ -0,0 +1,10 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by galaxywizkid on 11/4/16.
+ */
+
+public class StocksWidgetProvider extends AppWidgetProvider {
+}
