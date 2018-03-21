9a3ff86f2ca882af11b82df7380306756d5c8a39
Victor Akap Oben <victor@spoton.com>
2018-03-21 18:37:10 -0500

    Updated buildscript for fastlane

--- build.sh
+++ build.sh
@@ -1,3 +1,27 @@
 #!/bin/bash -e
 
+
 echo "Executing build.sh"
+
+export CI_LANE_DEV=${CI_LANE_DEV:="alpha"}
+export CI_LANE_STAGE=${CI_LANE_STAGE:="beta"}
+export CI_LANE_PROD=${CI_LANE_PROD:="deploy"}
+
+############################################
+## Define custom fastlane parameters here ##
+############################################
+
+
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+
+if exists fastlane; then
+    echo ">> Found existing version of fastlane"
+    fastlane -v
+else
+    echo ">> fastlane not found"
+    echo ">> install fastlane now? "
+
+fi
